58aab721ed5717e6b46caed6a58f9c660b21c5f5
pratu <pratu@gmail.com>
2024-05-03 13:18:53 +0000

    Edited in branch

---
+++ aws1/f1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo " Enter marks "
+read marks
+if [ $marks -gt 90 ]
+then
+	echo " Distinction "
+
+elif [ $marks -gt 75 ] && [ $marks -le 90 ]
+then
+	echo " first class "
+
+elif [ $marks -gt 60 ] && [ $marks -le 75 ]
+then 
+	echo " second class "
+
+elif [ $marks -gt 35 ] && [ $marks -le 60 ]
+then
+	echo " pass "
+
+else
+	echo " fail "
+
+fi
